1ad7aa4e03687ac64908b96bb789b37a99b41a44
Fernando Posser Pinheiro <fernando.posser@hotmail.com>
2021-05-03 11:37:29 -0300

    flask_mongoengine configured

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
 from flask import Flask
 from flask import request
+from flask_mongoengine import MongoEngine
+import mongoengine as me
 
-app = Flask("Mymba Feeder")
+app = Flask(__name__)
+
+app.config["MONGODB_SETTINGS"] = {
+    "db": "mymba-feeder-teste",
+    "host": "mongo",
+    "username": "root",
+    "password": "example",
+    'authentication_source': 'admin'
+}
+
+db = MongoEngine(app)
 
 
-@app.route("/", methods=["GET"])
+class HotspotModel(me.Document):
+    title = me.StringField(required=True)
+    contributors = me.ListField()
+    latitude = me.FloatField()
+    longitude = me.FloatField()
+
+    def __str__(self):
+        return self.title
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         return "Hello World"
+    else:
+        return "GETTTTT"
 
 
 @app.route("/<name>")
