bf69672df425edb90552a9adab1f3ff814963b1d
Fernando Posser Pinheiro <fernando.posser@hotmail.com>
2021-05-28 09:46:29 -0300

    app refatored fallowing flask best practices: folder with app name created | app factory function created | dev and prod requirements created | setup for installing created

---
+++ mymba-feeder/app.py
@@ -0,0 +1,51 @@
+import os
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import make_response
+
+from .models import db, HotspotModel
+from .data_manager import DataManager
+
+
+def create_app():
+
+    app = Flask(__name__)
+
+
+    app.config["MONGODB_SETTINGS"] = {
+        "db": os.getenv("DATABASE_NAME", "mymbafeeder"),
+        "host": os.getenv("DATABASE_HOST", "localhost"),
+        "username": os.getenv("DATABASE_USER", "root"),
+        "password": os.getenv("DATABASE_PASSWORD", "example"),
+        'authentication_source': 'admin'
+    }
+
+
+    db.init_app(app)
+
+
+    @app.route("/hotspot", methods=["GET", "POST"])
+    def index():
+        if request.method == "POST":   
+            response = make_response(
+                jsonify(
+                    DataManager().insert(request.get_json())), 
+                    200)
+            return response
+        else:
+            return make_response(
+                jsonify(HotspotModel.objects()),
+                200)
+
+
+    @app.route("/<name>")
+    def name(name):
+        return "hello {}".format(name)
+
+    return app
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
---
+++ mymba-feeder/models.py
@@ -0,0 +1,15 @@
+import datetime
+from flask_mongoengine import MongoEngine
+
+db = MongoEngine()
+
+class HotspotModel(db.Document):
+    title = db.StringField(required=True, unique=True)
+    description = db.StringField()
+    sponsors = db.ListField()
+    contributors = db.ListField()
+    position = db.DictField()
+    created = db.DateTimeField(default=datetime.datetime.now)
+
+    def __str__(self):
+        return self.title
